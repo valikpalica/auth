681171fc3afb6f8b5c09afa312e1a8329e8b1e05
valikpalica <palitsavalentin@gmail.com>
2022-01-24 17:59:24 +0200

    save changes

--- route.js
+++ route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controller = require('./controller');
 const {body} = require('express-validator')
 const authMiddleware = require('./middleware/tokenVerife');
-const gateway = require('./gateway/gateway');
 
 
 
